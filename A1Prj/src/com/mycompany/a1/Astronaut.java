96ff23399cb2894a30ebdaab9a5bf3d1652ef0d8
Aaron Enberg <aaron@debian>
2017-09-21 10:51:21 -0700

    add astronaut and interface for guided movement

---
+++ A1Prj/src/com/mycompany/a1/Astronaut.java
@@ -0,0 +1,12 @@
+package com.mycompany.a1;
+
+
+public class
+Astronaut extends Opponent
+{
+    public
+    Astronaut()
+    {
+        super();
+    }
+}
---
+++ A1Prj/src/com/mycompany/a1/IGuided.java
@@ -0,0 +1,21 @@
+package com.mycompany.a1;
+
+
+public interface IGuided {
+
+    public void
+    moveLeft();
+
+    public void
+    moveRight();
+
+    public void
+    moveUp();
+
+    public void
+    moveDown();
+
+    public void
+    jumpToLocation();
+
+}
