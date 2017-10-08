97f67ddb5d97aaead0fe7f0d89254b5fd0d895f3
Aaron Enberg <aaron@debian>
2017-10-08 15:31:53 -0700

    add keyCodes, key bindings, toggle sound in side menu

--- A2Prj/src/com/mycompany/a2/Game.java
+++ A2Prj/src/com/mycompany/a2/Game.java
@@ -25,45 +25,60 @@ import com.codename1.ui.Toolbar;
 public class
 Game extends Form
 {
-
     private GameWorld gameWorld = new GameWorld();
     private ScoreView scoreView;
     private MapView mapView;
-
-    private Button expandDoorButton = new Button();
-    private Button upButton = new Button();
-    private Button leftButton = new Button();
-    private Button spaceshipToAstronautButton = new Button();
-    private Button contractDoorButton = new Button();
-    private Button downButton = new Button();
-    private Button rightButton = new Button();
-    private Button spaceshipToAlienButton = new Button();
-    private Button rescueButton = new Button();
-    private Button newAlienButton = new Button();
-    private Button fightButton = new Button();
-    private Button tickButton = new Button();
     
-    private ExpandDoorCommand expandDoorCommand = new ExpandDoorCommand(gameWorld);
-    private MoveSpaceshipUpCommand moveSpaceshipUpCommand = new MoveSpaceshipUpCommand(gameWorld);
-    private MoveSpaceshipLeftCommand moveSpaceshipLeftCommand = new MoveSpaceshipLeftCommand(gameWorld);
+    private static int eKeyCode = 101;
+    private static int cKeyCode =  99;
+    private static int sKeyCode = 115;
+    private static int rKeyCode = 114;
+    private static int lKeyCode = 108;
+    private static int uKeyCode = 117;
+    private static int dKeyCode = 100;
+    private static int oKeyCode = 111;
+    private static int aKeyCode =  97;
+    private static int wKeyCode = 119;
+    private static int fKeyCode = 102;
+    private static int tKeyCode = 116;
+    private static int xKeyCode = 120;
+
+    private Button expandDoorButton           = new Button();
+    private Button upButton                   = new Button();
+    private Button leftButton                 = new Button();
+    private Button spaceshipToAstronautButton = new Button();
+    private Button contractDoorButton         = new Button();
+    private Button downButton                 = new Button();
+    private Button rightButton                = new Button();
+    private Button spaceshipToAlienButton     = new Button();
+    private Button rescueButton               = new Button();
+    private Button newAlienButton             = new Button();
+    private Button fightButton                = new Button();
+    private Button tickButton                 = new Button();
+
+    private ExpandDoorCommand           expandDoorCommand           = new ExpandDoorCommand(gameWorld);
+    private MoveSpaceshipUpCommand      moveSpaceshipUpCommand      = new MoveSpaceshipUpCommand(gameWorld);
+    private MoveSpaceshipLeftCommand    moveSpaceshipLeftCommand    = new MoveSpaceshipLeftCommand(gameWorld);
     private SpaceshipToAstronautCommand spaceshipToAstronautCommand = new SpaceshipToAstronautCommand(gameWorld);
-    private ContractDoorCommand contractDoorCommand = new ContractDoorCommand(gameWorld);
-    private MoveSpaceshipDownCommand moveSpaceshipDownCommand = new MoveSpaceshipDownCommand(gameWorld);
-    private MoveSpaceshipRightCommand moveSpaceshipRightCommand = new MoveSpaceshipRightCommand(gameWorld);
-    private SpaceshipToAlienCommand spaceshipToAlienCommand = new SpaceshipToAlienCommand(gameWorld);
-    private RescueCommand rescueCommand = new RescueCommand(gameWorld);
-    private NewAlienCommand newAlienCommand = new NewAlienCommand(gameWorld);
-    private FightCommand fightCommand = new FightCommand(gameWorld);
-    private TickCommand tickCommand = new TickCommand(gameWorld);
+    private ContractDoorCommand         contractDoorCommand         = new ContractDoorCommand(gameWorld);
+    private MoveSpaceshipDownCommand    moveSpaceshipDownCommand    = new MoveSpaceshipDownCommand(gameWorld);
+    private MoveSpaceshipRightCommand   moveSpaceshipRightCommand   = new MoveSpaceshipRightCommand(gameWorld);
+    private SpaceshipToAlienCommand     spaceshipToAlienCommand     = new SpaceshipToAlienCommand(gameWorld);
+    private RescueCommand               rescueCommand               = new RescueCommand(gameWorld);
+    private NewAlienCommand             newAlienCommand             = new NewAlienCommand(gameWorld);
+    private FightCommand                fightCommand                = new FightCommand(gameWorld);
+    private TickCommand                 tickCommand                 = new TickCommand(gameWorld);
+//    private exitCommand                 exitCommand                 = new ExitCommand(gameWorld);
 
     private Container controlWest;
     private Container controlEast;
     private Container controlSouth;
-    
+
     private Toolbar toolbar;
+    private ToggleSoundCommand toggleSound = new ToggleSoundCommand(gameWorld);
     private Command sideMenuItem1;
     private Command helpClickable;
-	
+
     public
     Game()
     {
@@ -75,7 +90,13 @@ Game extends Form
         gameWorld.addObserver(scoreView);
         gameWorld.addObserver(mapView);
         gameWorld.notifyObservers();
-        
+        System.out.println("''''''''''''''''''''''''''''''''''''''" +
+                           "''''''''''''''''''''''''''''''''''''''\n");
+
+        toolbar = new Toolbar();
+        this.setToolbar(toolbar);
+        this.setLayout(new BorderLayout());
+
         scoreView.setLayout(new FlowLayout(Component.CENTER));
         scoreView.getAllStyles().setPaddingTop(10);
         scoreView.getAllStyles().setBorder(Border.createCompoundBorder(
@@ -83,15 +104,21 @@ Game extends Form
                                            Border.createLineBorder(1),
                                            Border.createLineBorder(0),
                                            Border.createLineBorder(0)));
+
+        this.addKeyListener(eKeyCode, expandDoorCommand);
+        this.addKeyListener(cKeyCode, contractDoorCommand);
+        this.addKeyListener(sKeyCode, rescueCommand);
+        this.addKeyListener(rKeyCode, moveSpaceshipRightCommand);
+        this.addKeyListener(lKeyCode, moveSpaceshipLeftCommand);
+        this.addKeyListener(uKeyCode, moveSpaceshipUpCommand);
+        this.addKeyListener(dKeyCode, moveSpaceshipDownCommand);
+        this.addKeyListener(oKeyCode, spaceshipToAstronautCommand);
+        this.addKeyListener(aKeyCode, spaceshipToAlienCommand);
+        this.addKeyListener(wKeyCode, newAlienCommand);
+        this.addKeyListener(fKeyCode, fightCommand);
+        this.addKeyListener(tKeyCode, tickCommand);
+//        this.addKeyListener(xKeyCode, exitCommand);
         
-
-        controlWest = new Container(new BoxLayout(BoxLayout.Y_AXIS));
-        controlWest.getAllStyles().setBorder(Border.createCompoundBorder(
-                                             Border.createLineBorder(0),
-                                             Border.createLineBorder(0),
-                                             Border.createLineBorder(0),
-                                             Border.createLineBorder(1)));
-
         expandDoorButton.setCommand(expandDoorCommand);
         upButton.setCommand(moveSpaceshipUpCommand);
         leftButton.setCommand(moveSpaceshipLeftCommand);
@@ -104,65 +131,61 @@ Game extends Form
         newAlienButton.setCommand(newAlienCommand);
         fightButton.setCommand(fightCommand);
         tickButton.setCommand(tickCommand);
-        
-        
+
+        controlWest = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        controlWest.getAllStyles().setPaddingTop(100);
+        controlWest.getAllStyles().setBorder(Border.createCompoundBorder(
+                                             Border.createLineBorder(0),
+                                             Border.createLineBorder(0),
+                                             Border.createLineBorder(0),
+                                             Border.createLineBorder(1)));
+
         controlEast = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        controlEast.getAllStyles().setPaddingTop(100);
         controlEast.getAllStyles().setBorder(Border.createCompoundBorder(
                                              Border.createLineBorder(0),
                                              Border.createLineBorder(0),
                                              Border.createLineBorder(1),
                                              Border.createLineBorder(0)));
-        
-        
-        
-        
-        
-        
+
         controlSouth = new Container(new FlowLayout(Component.CENTER));
         controlSouth.getAllStyles().setBorder(Border.createCompoundBorder(
                                               Border.createLineBorder(1),
                                               Border.createLineBorder(0),
                                               Border.createLineBorder(0),
                                               Border.createLineBorder(0)));
-        
-        this.setLayout(new BorderLayout());
-        this.setTitle("Space Fights Game");
 
-        controlWest.getAllStyles().setPaddingTop(100);
         controlWest.add(expandDoorButton);
         controlWest.add(upButton);
         controlWest.add(leftButton);
         controlWest.add(spaceshipToAstronautButton);
-        
-        controlEast.getAllStyles().setPaddingTop(100);
+
         controlEast.add(contractDoorButton);
         controlEast.add(downButton);
         controlEast.add(rightButton);
         controlEast.add(spaceshipToAlienButton);
         controlEast.add(rescueButton);
-        
+
         controlSouth.add(newAlienButton);
         controlSouth.add(fightButton);
         controlSouth.add(tickButton);
+
         
-        toolbar = new Toolbar();
-        this.setToolbar(toolbar);
         toolbar.getAllStyles().setPaddingTop(40);
         toolbar.setTitle("Space Fights Game");
         toolbar.setTitleCentered(true);
         sideMenuItem1 = new Command("test");
         helpClickable = new Command("Help");
-        toolbar.addCommandToSideMenu(sideMenuItem1);
+        toolbar.addCommandToSideMenu(toggleSound);
         toolbar.addCommandToRightBar(helpClickable);
 
         this.addComponent(BorderLayout.NORTH, scoreView);
         this.addComponent(BorderLayout.WEST, controlWest);
         this.addComponent(BorderLayout.EAST, controlEast);
         this.addComponent(BorderLayout.SOUTH, controlSouth);
-        
+
         this.show();
     }
-	
 //    private void
 //    play()
 //    {
