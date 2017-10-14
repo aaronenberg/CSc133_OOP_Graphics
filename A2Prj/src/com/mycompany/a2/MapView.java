6975ac80c857e83a2e70dcc1fdc1771edcf8e849
aaron <aaron@debian.aaron>
2017-10-13 18:20:32 -0700

    remove height width variables and accessors

--- A2Prj/src/com/mycompany/a2/MapView.java
+++ A2Prj/src/com/mycompany/a2/MapView.java
@@ -3,21 +3,12 @@ package com.mycompany.a2;
 import java.util.Observable;
 import java.util.Observer;
 
+import com.codename1.charts.util.ColorUtil;
 import com.codename1.ui.Container;
 
 public class
 MapView extends Container implements Observer
 {
-    private int height;
-    private int width;
-
-    public
-    MapView()
-    {
-        height = this.getHeight();
-        width = this.getWidth();
-    }
-
     public void
     update(Observable observable, Object data)
     {
@@ -26,15 +17,4 @@ MapView extends Container implements Observer
         System.out.println();
     }
 
-    public int
-    mapHeight()
-    {
-        return this.height;
-    }
-
-    public int
-    mapWidth()
-    {
-        return this.width;
-    }
 }
