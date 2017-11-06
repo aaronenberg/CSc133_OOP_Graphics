b84be84f5c891ed5550b5c2a7743d592cf78f7b2
aaronenberg <pastaaroni@gmail.com>
2017-11-06 09:36:28 -0800

    calc origin based on center; change color, speed mult

--- A3Prj/src/com/mycompany/a3/Astronaut.java
+++ A3Prj/src/com/mycompany/a3/Astronaut.java
@@ -1,5 +1,6 @@
 package com.mycompany.a3;
 
+
 import com.codename1.charts.models.Point;
 import com.codename1.charts.util.ColorUtil;
 import com.codename1.ui.Graphics;
@@ -16,21 +17,23 @@ import com.codename1.ui.Graphics;
 public class
 Astronaut extends Opponent
 {
-    private static final int INIT_HEALTH   = 5,
-                             ATTACK_DAMAGE = 1,
-                             FADE_GREEN    = 15,
-                             HINT_OF_RED   = 15,
-                             DARK_GREEN    = 128,
-                             ZERO_BLUE     = 0;
+    private static final int
+            INIT_HEALTH = 5,
+            SPEED_MULTIPLIER = 20,
+            ALIEN_ATTACK_DAMAGE = 1,
+            FADE_COLOR = 25,
+            HINT_OF_RED = 15,
+            HINT_OF_GREEN = 15,
+            MEDIUM_BLUE = 128;
     private int health;
 
     public
     Astronaut()
     {
         super();
-        super.setColor(HINT_OF_RED, DARK_GREEN, ZERO_BLUE);
+        super.setColor(HINT_OF_RED, HINT_OF_GREEN, MEDIUM_BLUE);
         health = INIT_HEALTH;
-        super.setSpeed(health);
+        super.setSpeed(health*SPEED_MULTIPLIER);
     }
     
     public int
@@ -42,62 +45,66 @@ Astronaut extends Opponent
     private void
     updateHealth()
     {
-        if (health != 0)
-            health -= ATTACK_DAMAGE;
+            health -= ALIEN_ATTACK_DAMAGE;
     }
     private void
     updateSpeed()
     {
-        super.setSpeed(health);
+        super.setSpeed(health*SPEED_MULTIPLIER);
     }
     
     private void
     updateColor(int r, int g, int b)
     {
-        setColor(r, g + FADE_GREEN, b);   
+        setColor(r + FADE_COLOR, g + FADE_COLOR, b + FADE_COLOR);   
     }
 
     /* GameWorld.fight() calls this method with which
      * we are pretending a collision between alien
      * and astronaut has occurred. The collision
-     * damages the astronaut, decreasing it's health by 1,
-     * changes it's color, and decreases it's speed by 1.
+     * damages the astronaut, decreasing it's health,
+     * speed, and fading it's color.
      */
     public void
     collidesWithAlien()
     {
-        updateHealth();
-        updateColor(ColorUtil.red(getColor()),
+        if (health != 0) {
+            updateHealth();
+            updateColor(
+                    ColorUtil.red(getColor()),
                     ColorUtil.green(getColor()),
-                    ColorUtil.blue(getColor()));
-        updateSpeed();
+                    ColorUtil.blue(getColor())
+            );
+            updateSpeed();
+        }
     }
 
+     // Astronauts with zero health left can not move, they are dead.
+    @Override
+    public void
+    move(int elapsedMilliSecs)
+    {
+        if (health != 0)
+            super.move(elapsedMilliSecs);
+    }
+
+    public void
+    draw(Graphics g, Point pCmpRelPrnt)
+    {
+        int base = getSize();
+        int xCenter = (int) (pCmpRelPrnt.getX() + getX());
+        int yCenter = (int) (pCmpRelPrnt.getY() + getY());
+        int xOrigin = xCenter - base/2;
+        int yOrigin = yCenter + base/2;
+        g.setColor(getColor());
+        g.fillTriangle(xOrigin+base/2, yOrigin+base/2, xOrigin, yOrigin-base, xOrigin+base, yOrigin-base);
+    }
+
+    @Override
     public String
     toString()
     {
         String opponentString = super.toString();
         return "Astronaut: " + opponentString + " health=" + health;
     }
-    
-
-     // Astronauts with zero health left can not move, they are dead.
-    public void
-    move()
-    {
-        if (health != 0)
-            super.move();
-    }
-
-    public void
-    draw(Graphics g, Point pCmpRelPrnt)
-    {
-        int x = (int) (pCmpRelPrnt.getX() + getX());
-        int y =  (int) (pCmpRelPrnt.getY() + getY());
-        int base = getSize();
-//        g.drawRect(x, y, getSize(), getSize());
-        g.setColor(getColor());
-        g.fillTriangle(x+base/2, y+base/2, x, y-base, x+base, y-base);
-    }
-
 }
