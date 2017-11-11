bf45ad88a72e4e5c6d3e2f020c332178910f5f52
aaronenberg <pastaaroni@gmail.com>
2017-11-11 15:21:43 -0800

    check and handle collsions in run method

--- A3Prj/src/com/mycompany/a3/Game.java
+++ A3Prj/src/com/mycompany/a3/Game.java
@@ -10,7 +10,6 @@ import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.plaf.Border;
 import com.codename1.ui.util.UITimer;
-import java.lang.Runnable;
 
 
 /*
@@ -26,51 +25,50 @@ import java.lang.Runnable;
 public class
 Game extends Form implements Runnable
 {
-    private GameWorld      gameWorld = new GameWorld();
-    private ScoreView      scoreView;
+    private GameWorld gameWorld = new GameWorld();
+    private ScoreView scoreView;
     private static MapView mapView;
-    private int            mapHeight;
-    private int            mapWidth;
+    private int
+        mapHeight,
+        mapWidth;
     private static final int elapsedMilliSecs = 20;
 
     private static int 
-            eKeyCode = 101,
-            cKeyCode =  99,
-            sKeyCode = 115,
-            rKeyCode = 114,
-            lKeyCode = 108,
-            uKeyCode = 117,
-            dKeyCode = 100,
-            oKeyCode = 111,
-            aKeyCode =  97,
-            wKeyCode = 119,
-            fKeyCode = 102,
-            xKeyCode = 120;
+        eKeyCode = 101,
+        cKeyCode =  99,
+        sKeyCode = 115,
+        rKeyCode = 114,
+        lKeyCode = 108,
+        uKeyCode = 117,
+        dKeyCode = 100,
+        oKeyCode = 111,
+        aKeyCode =  97,
+        xKeyCode = 120;
 
     private Button 
-            expandDoorButton           = new Button(),
-            upButton                   = new Button(),
-            leftButton                 = new Button(),
-            spaceshipToAstronautButton = new Button(),
-            contractDoorButton         = new Button(),
-            downButton                 = new Button(),
-            rightButton                = new Button(),
-            spaceshipToAlienButton     = new Button(),
-            rescueButton               = new Button(),
-            newAlienButton             = new Button(),
-            fightButton                = new Button();
+        expandDoorButton           = new Button(),
+        upButton                   = new Button(),
+        leftButton                 = new Button(),
+        spaceshipToAstronautButton = new Button(),
+        contractDoorButton         = new Button(),
+        downButton                 = new Button(),
+        rightButton                = new Button(),
+        spaceshipToAlienButton     = new Button(),
+        rescueButton               = new Button();
+//            healButton                 = new Button(),
+//            pauseButton                = new Button();
 
-    private ExpandDoorCommand           expandDoorCommand           = new ExpandDoorCommand(gameWorld);
+    private ExpandDoorCommand           expandDoorCommand           = new ExpandDoorCommand(gameWorld, eKeyCode);
     private MoveSpaceshipUpCommand      moveSpaceshipUpCommand      = new MoveSpaceshipUpCommand(gameWorld);
     private MoveSpaceshipLeftCommand    moveSpaceshipLeftCommand    = new MoveSpaceshipLeftCommand(gameWorld);
     private SpaceshipToAstronautCommand spaceshipToAstronautCommand = new SpaceshipToAstronautCommand(gameWorld);
-    private ContractDoorCommand         contractDoorCommand         = new ContractDoorCommand(gameWorld);
+    private ContractDoorCommand         contractDoorCommand         = new ContractDoorCommand(gameWorld, cKeyCode);
     private MoveSpaceshipDownCommand    moveSpaceshipDownCommand    = new MoveSpaceshipDownCommand(gameWorld);
     private MoveSpaceshipRightCommand   moveSpaceshipRightCommand   = new MoveSpaceshipRightCommand(gameWorld);
     private SpaceshipToAlienCommand     spaceshipToAlienCommand     = new SpaceshipToAlienCommand(gameWorld);
     private RescueCommand               rescueCommand               = new RescueCommand(gameWorld);
-    private NewAlienCommand             newAlienCommand             = new NewAlienCommand(gameWorld);
-    private FightCommand                fightCommand                = new FightCommand(gameWorld);
+//    private HealCommand                 healCommand                 = new HealCommand(gameWorld);
+//    private PauseCommand                pauseCommand                = new PauseCommand(gameWorld);
     private ToggleSoundCommand          toggleSound                 = new ToggleSoundCommand(gameWorld);
     private ExitCommand                 exitCommand                 = new ExitCommand();
     private HelpCommand                 helpCommand                 = new HelpCommand();
@@ -78,9 +76,10 @@ Game extends Form implements Runnable
 
     private CheckBox sideMenuCheckBox = new CheckBox("Sound");
 
-    private Container controlWest;
-    private Container controlEast;
-    private Container controlSouth;
+    private Container
+        controlWest,
+        controlEast,
+        controlSouth;
 
     private Toolbar toolbar;
     private UITimer timer;
@@ -97,23 +96,21 @@ Game extends Form implements Runnable
         timer = new UITimer(this);
 
         toolbar = new Toolbar();
-        this.setToolbar(toolbar);
-        this.setLayout(new BorderLayout());
+        setToolbar(toolbar);
+        setLayout(new BorderLayout());
 
         scoreView.setLayout(new FlowLayout(Component.CENTER));
 
-        this.addKeyListener(eKeyCode, expandDoorCommand);
-        this.addKeyListener(cKeyCode, contractDoorCommand);
-        this.addKeyListener(sKeyCode, rescueCommand);
-        this.addKeyListener(rKeyCode, moveSpaceshipRightCommand);
-        this.addKeyListener(lKeyCode, moveSpaceshipLeftCommand);
-        this.addKeyListener(uKeyCode, moveSpaceshipUpCommand);
-        this.addKeyListener(dKeyCode, moveSpaceshipDownCommand);
-        this.addKeyListener(oKeyCode, spaceshipToAstronautCommand);
-        this.addKeyListener(aKeyCode, spaceshipToAlienCommand);
-        this.addKeyListener(wKeyCode, newAlienCommand);
-        this.addKeyListener(fKeyCode, fightCommand);
-        this.addKeyListener(xKeyCode, exitCommand);
+        addKeyListener(eKeyCode, expandDoorCommand);
+        addKeyListener(cKeyCode, contractDoorCommand);
+        addKeyListener(sKeyCode, rescueCommand);
+        addKeyListener(rKeyCode, moveSpaceshipRightCommand);
+        addKeyListener(lKeyCode, moveSpaceshipLeftCommand);
+        addKeyListener(uKeyCode, moveSpaceshipUpCommand);
+        addKeyListener(dKeyCode, moveSpaceshipDownCommand);
+        addKeyListener(oKeyCode, spaceshipToAstronautCommand);
+        addKeyListener(aKeyCode, spaceshipToAlienCommand);
+        addKeyListener(xKeyCode, exitCommand);
 
         expandDoorButton.setCommand(expandDoorCommand);
         upButton.setCommand(moveSpaceshipUpCommand);
@@ -124,8 +121,8 @@ Game extends Form implements Runnable
         rightButton.setCommand(moveSpaceshipRightCommand);
         spaceshipToAlienButton.setCommand(spaceshipToAlienCommand);
         rescueButton.setCommand(rescueCommand);
-        newAlienButton.setCommand(newAlienCommand);
-        fightButton.setCommand(fightCommand);
+//        healButton.setCommand(healCommand);
+//        pauseButton.setCommand(pauseCommand);
         sideMenuCheckBox.setCommand(toggleSound);
 
         controlWest = new Container(new BoxLayout(BoxLayout.Y_AXIS));
@@ -162,8 +159,8 @@ Game extends Form implements Runnable
         controlEast.add(spaceshipToAlienButton);
         controlEast.add(rescueButton);
 
-        controlSouth.add(newAlienButton);
-        controlSouth.add(fightButton);
+//        controlSouth.add(healButton);
+//        controlSouth.add(pauseButton);
 
         toolbar.getAllStyles().setPaddingTop(40);
         toolbar.setTitle("Space Fights Game");
@@ -175,13 +172,13 @@ Game extends Form implements Runnable
         toolbar.addCommandToSideMenu(aboutCommand);
         toolbar.addCommandToRightBar(helpCommand);
 
-        this.addComponent(BorderLayout.NORTH, scoreView);
-        this.addComponent(BorderLayout.WEST, controlWest);
-        this.addComponent(BorderLayout.EAST, controlEast);
-        this.addComponent(BorderLayout.SOUTH, controlSouth);
-        this.addComponent(BorderLayout.CENTER, mapView);
+        addComponent(BorderLayout.NORTH, scoreView);
+        addComponent(BorderLayout.WEST, controlWest);
+        addComponent(BorderLayout.EAST, controlEast);
+        addComponent(BorderLayout.SOUTH, controlSouth);
+        addComponent(BorderLayout.CENTER, mapView);
 
-        this.show();
+        show();
         /*
          * container dimensions are set only after calling show()
          * so we query them here and set locations of each game object
@@ -217,5 +214,6 @@ Game extends Form implements Runnable
     run()
     {
         gameWorld.tick(elapsedMilliSecs);
+        gameWorld.checkForAndHandleCollisions();
     }
 }
