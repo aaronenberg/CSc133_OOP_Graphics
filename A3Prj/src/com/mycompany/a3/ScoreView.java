b90b07c59a6f693419264d82b5727c8c6b0c175f
aaronenberg <pastaaroni@gmail.com>
2017-11-13 11:12:15 -0800

    moved styling and adding labels,values to private methods; when game ends, stop timer, show dialog with final score and exit

--- A3Prj/src/com/mycompany/a3/ScoreView.java
+++ A3Prj/src/com/mycompany/a3/ScoreView.java
@@ -2,6 +2,7 @@ package com.mycompany.a3;
 
 
 import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
 import com.codename1.ui.Display;
 import com.codename1.ui.Label;
 import com.codename1.ui.plaf.Border;
@@ -26,8 +27,50 @@ ScoreView extends Container implements Observer
         soundLabel               = new Label(),
         soundValue               = new Label();
 
+    private String exit = "Exit";
+    private String finalScoreDialogTitle = "Winner";
+    private String finalScoreDialogBody = "All astronauts have been rescued!\n"
+                                          + "The spaceship flies back into "
+                                          + "space.\n\nYour final score: ";
+
     public
     ScoreView()
+    {
+        setScoreViewLabelsAndValues();
+        styleScoreView();
+    }
+
+    public void
+    update(Observable observable, Object arg)
+    {
+        totalScoreValue.setText(         "" + GameWorld.getScore());
+        astronautsRescuedValue.setText(  "" + GameWorld.getAstronautsRescued());
+        aliensSnuckInValue.setText(      "" + GameWorld.getAliensSnuckIn());
+        astronautsRemainingValue.setText("" + GameWorld.getAstronautsRemaining());
+        aliensRemainingValue.setText(    "" + GameWorld.getAliensRemaining());
+        soundValue.setText(              "" + checkSoundStatus(GameWorld.getSound()));
+
+        repaint();
+        checkGameOver();
+    }
+
+    public String
+    checkSoundStatus(Boolean soundOn)
+    {
+        if (soundOn)
+            return "ON";
+        else
+            return "OFF";
+    }
+
+    public static String
+    getSoundText()
+    {
+        return soundLabel.getText() + soundValue.getText();
+    }
+
+    private void
+    setScoreViewLabelsAndValues()
     {
         totalScoreValue.getAllStyles().setMarginRight(20);
         astronautsRescuedValue.getAllStyles().setMarginRight(20);
@@ -48,13 +91,6 @@ ScoreView extends Container implements Observer
         add(soundLabel);
         add(soundValue);
 
-        getAllStyles().setPaddingTop(10);
-        getAllStyles().setBorder(Border.createCompoundBorder(
-                                 Border.createLineBorder(1),
-                                 Border.createLineBorder(1),
-                                 Border.createLineBorder(0),
-                                 Border.createLineBorder(0)));
-
         totalScoreLabel.setText("Total Score:");
         astronautsRescuedLabel.setText("Astronauts Rescued:");
         aliensSnuckInLabel.setText("Aliens Snuck In:");
@@ -63,41 +99,29 @@ ScoreView extends Container implements Observer
         soundLabel.setText("Sound:");
     }
 
-    public void
-    update(Observable observable, Object arg)
+    private void
+    styleScoreView()
     {
-        totalScoreValue.setText(         "" + GameWorld.getScore());
-        astronautsRescuedValue.setText(  "" + GameWorld.getAstronautsRescued());
-        aliensSnuckInValue.setText(      "" + GameWorld.getAliensSnuckIn());
-        astronautsRemainingValue.setText("" + GameWorld.getAstronautsRemaining());
-        aliensRemainingValue.setText(    "" + GameWorld.getAliensRemaining());
-        soundValue.setText(              "" + checkSoundStatus(GameWorld.getSound()));
+        getAllStyles().setPaddingTop(10);
+        getAllStyles().setBorder(Border.createCompoundBorder(
+                                 Border.createLineBorder(1),
+                                 Border.createLineBorder(1),
+                                 Border.createLineBorder(0),
+                                 Border.createLineBorder(0)));
+    }
 
-        repaint();
-
+    private void
+    checkGameOver()
+    {
         if (GameWorld.getAstronautsRemaining() == 0) {
-            System.out.println("All astronauts have been rescued! " +
-                               "The spaceship flies back into space.\n");
-            System.out.println("Your final score: " + GameWorld.getScore());
-            System.out.println("\n''''''''''''''''''''''''''''''''''''''" +
-                               "''''''''''''''''''''''''''''''''''''''\n");
-            Display.getInstance().exitApplication();
+            Game.getUITimer().cancel();
+            Dialog.show(
+                finalScoreDialogTitle,
+                finalScoreDialogBody + GameWorld.getScore(),
+                exit, null
+             );
+             Display.getInstance().exitApplication();
         }
-    }
-
-    public String
-    checkSoundStatus(Boolean soundOn)
-    {
-        if (soundOn)
-            return "ON";
-        else
-            return "OFF";
-    }
-
-    public static String
-    getSoundText()
-    {
-        return soundLabel.getText() + soundValue.getText();
     }
 
 }
