2e56f450f5a123a61d6619df78233e5f368ef040
aaronenberg <pastaaroni@gmail.com>
2017-11-13 11:15:43 -0800

    add heal button, private methods for setting up control regions, toolbar; disabled buttons in pause mode.

--- A3Prj/src/com/mycompany/a3/Game.java
+++ A3Prj/src/com/mycompany/a3/Game.java
@@ -3,6 +3,8 @@ package com.mycompany.a3;
 import com.codename1.ui.CheckBox;
 import com.codename1.ui.Component;
 import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.Display;
 import com.codename1.ui.Form;
 import com.codename1.ui.Toolbar;
 import com.codename1.ui.layouts.BorderLayout;
@@ -33,6 +35,8 @@ Game extends Form implements Runnable
         mapWidth;
     private static final int elapsedMilliSecs = 20;
     private static boolean gamePaused = false;
+    private String  exit           = "Exit";
+    private String  finalScoreDialogBody = "All astronauts have been rescued!\nThe spaceship flies back into space.\n\n Your final score: \n" + GameWorld.getScore();
 
     private static int 
         eKeyCode = 101,
@@ -56,7 +60,7 @@ Game extends Form implements Runnable
         rightButton                = new Button(),
         spaceshipToAlienButton     = new Button(),
         rescueButton               = new Button(),
-//            healButton                 = new Button(),
+        healButton                 = new Button(),
         gameModeButton             = new Button();
 
     private ExpandDoorCommand           expandDoorCommand           = new ExpandDoorCommand(gameWorld, eKeyCode);
@@ -68,7 +72,7 @@ Game extends Form implements Runnable
     private MoveSpaceshipRightCommand   moveSpaceshipRightCommand   = new MoveSpaceshipRightCommand(gameWorld);
     private SpaceshipToAlienCommand     spaceshipToAlienCommand     = new SpaceshipToAlienCommand(gameWorld);
     private RescueCommand               rescueCommand               = new RescueCommand(gameWorld);
-//    private HealCommand                 healCommand                 = new HealCommand(gameWorld);
+    private HealCommand                 healCommand                 = new HealCommand(gameWorld);
     private GameModeCommand             gameModeCommand             = new GameModeCommand(this);
     private ToggleSoundCommand          toggleSound                 = new ToggleSoundCommand(gameWorld);
     private ExitCommand                 exitCommand                 = new ExitCommand();
@@ -83,7 +87,7 @@ Game extends Form implements Runnable
         controlSouth;
 
     private Toolbar toolbar;
-    private UITimer timer;
+    private static UITimer timer;
 
     public
     Game()
@@ -98,106 +102,33 @@ Game extends Form implements Runnable
 
         toolbar = new Toolbar();
         setToolbar(toolbar);
-        setLayout(new BorderLayout());
 
+        setLayout(new BorderLayout());
         scoreView.setLayout(new FlowLayout(Component.CENTER));
 
-        addKeyListener(eKeyCode, expandDoorCommand);
-        addKeyListener(cKeyCode, contractDoorCommand);
-        addKeyListener(sKeyCode, rescueCommand);
-        addKeyListener(rKeyCode, moveSpaceshipRightCommand);
-        addKeyListener(lKeyCode, moveSpaceshipLeftCommand);
-        addKeyListener(uKeyCode, moveSpaceshipUpCommand);
-        addKeyListener(dKeyCode, moveSpaceshipDownCommand);
-        addKeyListener(oKeyCode, spaceshipToAstronautCommand);
-        addKeyListener(aKeyCode, spaceshipToAlienCommand);
         addKeyListener(xKeyCode, exitCommand);
 
-        expandDoorButton.setCommand(expandDoorCommand);
-        upButton.setCommand(moveSpaceshipUpCommand);
-        leftButton.setCommand(moveSpaceshipLeftCommand);
-        spaceshipToAstronautButton.setCommand(spaceshipToAstronautCommand);
-        contractDoorButton.setCommand(contractDoorCommand);
-        downButton.setCommand(moveSpaceshipDownCommand);
-        rightButton.setCommand(moveSpaceshipRightCommand);
-        spaceshipToAlienButton.setCommand(spaceshipToAlienCommand);
-        rescueButton.setCommand(rescueCommand);
-//        healButton.setCommand(healCommand);
-        gameModeButton.setCommand(gameModeCommand);
-        sideMenuCheckBox.setCommand(toggleSound);
+        setButtonsDisabledStyle();
+        bindCommandsToButtons();
+        setPlayModeCommands();
+        setControlWest();
+        setControlEast();
+        setControlSouth();
+        styleToolbar();
+        addCommandsToToolbar();
+        addComponentsToControlRegions();
 
-        controlWest = new Container(new BoxLayout(BoxLayout.Y_AXIS));
-        controlWest.getAllStyles().setPaddingTop(100);
-        controlWest.getAllStyles().setBorder(Border.createCompoundBorder(
-                                             Border.createLineBorder(0),
-                                             Border.createLineBorder(0),
-                                             Border.createLineBorder(0),
-                                             Border.createLineBorder(1)));
-
-        controlEast = new Container(new BoxLayout(BoxLayout.Y_AXIS));
-        controlEast.getAllStyles().setPaddingTop(100);
-        controlEast.getAllStyles().setBorder(Border.createCompoundBorder(
-                                             Border.createLineBorder(0),
-                                             Border.createLineBorder(0),
-                                             Border.createLineBorder(1),
-                                             Border.createLineBorder(0)));
-
-        controlSouth = new Container(new FlowLayout(Component.CENTER));
-        controlSouth.getAllStyles().setBorder(Border.createCompoundBorder(
-                                              Border.createLineBorder(1),
-                                              Border.createLineBorder(0),
-                                              Border.createLineBorder(0),
-                                              Border.createLineBorder(0)));
-
-        controlWest.add(expandDoorButton);
-        controlWest.add(upButton);
-        controlWest.add(leftButton);
-        controlWest.add(spaceshipToAstronautButton);
-
-        controlEast.add(contractDoorButton);
-        controlEast.add(downButton);
-        controlEast.add(rightButton);
-        controlEast.add(spaceshipToAlienButton);
-        controlEast.add(rescueButton);
-
-//        controlSouth.add(healButton);
-        gameModeButton.getAllStyles().setPaddingRight(20);
-        gameModeButton.getAllStyles().setPaddingLeft(20);
-        controlSouth.add(gameModeButton);
-
-        toolbar.getAllStyles().setPaddingTop(40);
-        toolbar.setTitle("Space Fights Game");
-        toolbar.setTitleCentered(true);
-        toggleSound.putClientProperty("SideComponent", sideMenuCheckBox);
-        toolbar.addCommandToSideMenu(toggleSound);
-        toolbar.addCommandToSideMenu(exitCommand);
-        toolbar.addCommandToSideMenu(rescueCommand);
-        toolbar.addCommandToSideMenu(aboutCommand);
-        toolbar.addCommandToRightBar(helpCommand);
-
-        addComponent(BorderLayout.NORTH, scoreView);
-        addComponent(BorderLayout.WEST, controlWest);
-        addComponent(BorderLayout.EAST, controlEast);
-        addComponent(BorderLayout.SOUTH, controlSouth);
-        addComponent(BorderLayout.CENTER, mapView);
+        show();
 
         /*
          * container dimensions are set only after calling show()
          * so we query them here and set locations of each game object
-         * randomly within these limits, but not necessarily inside the
-         * MapView container yet.
+         * randomly within those limits.
          */
-        show();
-
         mapHeight = getMapHeight();
         mapWidth = getMapWidth();
         gameWorld.initLocationsOnMap();
-
-        System.out.println("MapView dimensions are: " +
-                            mapWidth + "x" + mapHeight +
-                            " (width X height)\n");
-        System.out.println("''''''''''''''''''''''''''''''''''''''" +
-                           "''''''''''''''''''''''''''''''''''''''\n");
+        printMapDimensions();
 
         timer.schedule(elapsedMilliSecs, true, this);
     }
@@ -214,6 +145,15 @@ Game extends Form implements Runnable
         return mapView.getWidth();
     }
 
+    private void
+    printMapDimensions()
+    {
+        System.out.println("MapView dimensions are: " +
+                mapWidth + "x" + mapHeight +
+                " (width X height)\n");
+        System.out.println("''''''''''''''''''''''''''''''''''''''" +
+                            "''''''''''''''''''''''''''''''''''''''\n");
+    }
     public void
     run()
     {
@@ -221,10 +161,17 @@ Game extends Form implements Runnable
         gameWorld.checkForAndHandleCollisions();
     }
 
+    public static UITimer
+    getUITimer()
+    {
+        return timer;
+    }
     public void
     resumeGame()
     {
         timer.schedule(elapsedMilliSecs, true, this);
+        GameWorld.clearSelected();
+        setPlayModeCommands();
         gamePaused = false;
         gameModeButton.setText("Pause");
         controlSouth.repaint();
@@ -235,6 +182,7 @@ Game extends Form implements Runnable
     {
         timer.cancel();
         gamePaused = true;
+        setPauseModeCommands();
         gameModeButton.setText("  Play  ");
         controlSouth.repaint();
     }
@@ -244,4 +192,185 @@ Game extends Form implements Runnable
     {
         return gamePaused;
     }
+
+    private void
+    setPauseModeCommands()
+    {
+        expandDoorButton.setEnabled(false);
+        upButton.setEnabled(false);
+        leftButton.setEnabled(false);
+        spaceshipToAstronautButton.setEnabled(false);
+        contractDoorButton.setEnabled(false);
+        downButton.setEnabled(false);
+        rightButton.setEnabled(false);
+        spaceshipToAlienButton.setEnabled(false);
+        rescueButton.setEnabled(false);
+        healButton.setEnabled(true);
+        rescueCommand.setEnabled(false);
+
+        removeKeyListener(eKeyCode, expandDoorCommand);
+        removeKeyListener(cKeyCode, contractDoorCommand);
+        removeKeyListener(sKeyCode, rescueCommand);
+        removeKeyListener(rKeyCode, moveSpaceshipRightCommand);
+        removeKeyListener(lKeyCode, moveSpaceshipLeftCommand);
+        removeKeyListener(uKeyCode, moveSpaceshipUpCommand);
+        removeKeyListener(dKeyCode, moveSpaceshipDownCommand);
+        removeKeyListener(oKeyCode, spaceshipToAstronautCommand);
+        removeKeyListener(aKeyCode, spaceshipToAlienCommand);
+    }
+
+    private void
+    setPlayModeCommands()
+    {
+        expandDoorButton.setEnabled(true);
+        upButton.setEnabled(true);
+        leftButton.setEnabled(true);
+        spaceshipToAstronautButton.setEnabled(true);
+        contractDoorButton.setEnabled(true);
+        downButton.setEnabled(true);
+        rightButton.setEnabled(true);
+        spaceshipToAlienButton.setEnabled(true);
+        rescueButton.setEnabled(true);
+        healButton.setEnabled(false);
+        gameModeButton.setEnabled(true);
+        rescueCommand.setEnabled(true);
+
+        addKeyListener(eKeyCode, expandDoorCommand);
+        addKeyListener(cKeyCode, contractDoorCommand);
+        addKeyListener(sKeyCode, rescueCommand);
+        addKeyListener(rKeyCode, moveSpaceshipRightCommand);
+        addKeyListener(lKeyCode, moveSpaceshipLeftCommand);
+        addKeyListener(uKeyCode, moveSpaceshipUpCommand);
+        addKeyListener(dKeyCode, moveSpaceshipDownCommand);
+        addKeyListener(oKeyCode, spaceshipToAstronautCommand);
+        addKeyListener(aKeyCode, spaceshipToAlienCommand);
+        
+    }
+
+    private void
+    setButtonsDisabledStyle()
+    {
+        expandDoorButton.getDisabledStyle().setBgTransparency(0);
+        expandDoorButton.getDisabledStyle().setFgColor(0);
+        upButton.getDisabledStyle().setBgTransparency(0);
+        upButton.getDisabledStyle().setFgColor(0);
+        leftButton.getDisabledStyle().setBgTransparency(0);
+        leftButton.getDisabledStyle().setFgColor(0);
+        spaceshipToAstronautButton.getDisabledStyle().setBgTransparency(0);
+        spaceshipToAstronautButton.getDisabledStyle().setFgColor(0);
+        contractDoorButton.getDisabledStyle().setBgTransparency(0);
+        contractDoorButton.getDisabledStyle().setFgColor(0);
+        downButton.getDisabledStyle().setBgTransparency(0);
+        downButton.getDisabledStyle().setFgColor(0);
+        rightButton.getDisabledStyle().setBgTransparency(0);
+        rightButton.getDisabledStyle().setFgColor(0);
+        spaceshipToAlienButton.getDisabledStyle().setBgTransparency(0);
+        spaceshipToAlienButton.getDisabledStyle().setFgColor(0);
+        rescueButton.getDisabledStyle().setBgTransparency(0);
+        rescueButton.getDisabledStyle().setFgColor(0);
+        healButton.getDisabledStyle().setBgTransparency(0);
+        healButton.getDisabledStyle().setFgColor(0);
+    }
+
+    private void
+    bindCommandsToButtons()
+    {
+        expandDoorButton.setCommand(expandDoorCommand);
+        upButton.setCommand(moveSpaceshipUpCommand);
+        leftButton.setCommand(moveSpaceshipLeftCommand);
+        spaceshipToAstronautButton.setCommand(spaceshipToAstronautCommand);
+        contractDoorButton.setCommand(contractDoorCommand);
+        downButton.setCommand(moveSpaceshipDownCommand);
+        rightButton.setCommand(moveSpaceshipRightCommand);
+        spaceshipToAlienButton.setCommand(spaceshipToAlienCommand);
+        rescueButton.setCommand(rescueCommand);
+        healButton.setCommand(healCommand);
+        gameModeButton.setCommand(gameModeCommand);
+        sideMenuCheckBox.setCommand(toggleSound);
+    }
+
+    private void
+    addComponentsToControlRegions()
+    {
+        addComponent(BorderLayout.NORTH, scoreView);
+        addComponent(BorderLayout.WEST, controlWest);
+        addComponent(BorderLayout.EAST, controlEast);
+        addComponent(BorderLayout.SOUTH, controlSouth);
+        addComponent(BorderLayout.CENTER, mapView);
+    }
+
+    private void
+    addCommandsToToolbar()
+    {
+        toggleSound.putClientProperty("SideComponent", sideMenuCheckBox);
+        toolbar.addCommandToSideMenu(toggleSound);
+        toolbar.addCommandToSideMenu(exitCommand);
+        toolbar.addCommandToSideMenu(rescueCommand);
+        toolbar.addCommandToSideMenu(aboutCommand);
+        toolbar.addCommandToRightBar(helpCommand);
+    }
+
+    private void
+    styleToolbar()
+    {
+        toolbar.getAllStyles().setPaddingTop(40);
+        toolbar.setTitle("Space Fights Game");
+        toolbar.setTitleCentered(true);
+    }
+
+    private void
+    setControlWest()
+    {
+        controlWest = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        controlWest.getAllStyles().setPaddingTop(100);
+        controlWest.getAllStyles().setBorder(Border.createCompoundBorder(
+                                             Border.createLineBorder(0),
+                                             Border.createLineBorder(0),
+                                             Border.createLineBorder(0),
+                                             Border.createLineBorder(1)));
+        controlWest.add(expandDoorButton);
+        controlWest.add(upButton);
+        controlWest.add(leftButton);
+        controlWest.add(spaceshipToAstronautButton);
+    }
+
+    private void
+    setControlEast()
+    {
+        controlEast = new Container(new BoxLayout(BoxLayout.Y_AXIS));
+        controlEast.getAllStyles().setPaddingTop(100);
+        controlEast.getAllStyles().setBorder(
+            Border.createCompoundBorder(
+                 Border.createLineBorder(0),
+                 Border.createLineBorder(0),
+                 Border.createLineBorder(1),
+                 Border.createLineBorder(0)
+            )
+        );
+        controlEast.add(contractDoorButton);
+        controlEast.add(downButton);
+        controlEast.add(rightButton);
+        controlEast.add(spaceshipToAlienButton);
+        controlEast.add(rescueButton);
+    }
+
+    private void
+    setControlSouth()
+    {
+        controlSouth = new Container(new FlowLayout(Component.CENTER));
+        controlSouth.getAllStyles().setBorder(
+            Border.createCompoundBorder(
+                 Border.createLineBorder(1),
+                 Border.createLineBorder(0),
+                 Border.createLineBorder(0),
+                 Border.createLineBorder(0)
+            )
+        );
+        gameModeButton.getAllStyles().setPaddingRight(20);
+        gameModeButton.getAllStyles().setPaddingLeft(20);
+        controlSouth.add(healButton);
+        controlSouth.add(gameModeButton);
+        
+    }
+
 }
